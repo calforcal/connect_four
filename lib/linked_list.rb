97996580a3ffe1bcd033463c3ef47f3f81603480
Michael Callahan <mcalla242@gmail.com>
2023-04-07 08:56:51 -0600

    Feat: Add get_node_value method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -14,4 +14,14 @@ class LinkedList
     end
     current_node.node_below = Node.new(node_value)
   end
+
+  def get_node_value(num)
+    current_node = @head
+    counter = 0
+    while counter < num
+      current_node = current_node.node_below
+      counter += 1
+    end
+    current_node.value
+  end
 end
