dd4be1673a0ef5afe213b14acbd653e4d835c521
Michael Callahan <mcalla242@gmail.com>
2023-04-06 21:19:45 -0600

    Test: Add testing specs for Node class

---
+++ spec/node_spec.rb
@@ -0,0 +1,18 @@
+require "./lib/node"
+
+RSpec.describe Node do
+  before(:each)
+    node = Node.new("X")
+  end
+
+  describe "#initialize" do
+    it "initializes" do
+      expect(node).to be_an_instance_of(Node)
+    end
+
+    it "initializes with attributes" do
+      expect(node.value).to eq("X")
+      expect(node.node_below).to eq(nil)
+    end
+  end
+end
