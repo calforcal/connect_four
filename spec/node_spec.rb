d8f4e9bf8eff1ee03c772c82a1904be408ebd789
Michael Callahan <mcalla242@gmail.com>
2023-04-06 21:38:19 -0600

    Test: Add testing spec to Node class

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -3,6 +3,7 @@ require "./lib/node"
 RSpec.describe Node do
   before(:each) do
     @node = Node.new("X")
+    @node_2 = Node.new("XX")
   end
   describe "initialize" do
     it "initializes" do
@@ -12,6 +13,11 @@ RSpec.describe Node do
     it "initializes with data" do
       expect(@node.value).to eq("X")
       expect(@node.node_below).to eq(nil)
+    end
+
+    it "can have a node_below" do
+      @node.node_below = @node_2
+      expect(@node.node_below).to eq(@node_2)
     end
   end
 end
