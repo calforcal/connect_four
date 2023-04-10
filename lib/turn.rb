a62958225684386d671e4bbc4ebb209ce91c05d0
Blaine Glasgow <glasgowb@outlook.com>
2023-04-10 14:35:16 -0700

    Make turn class and correct linked_list test file

---
+++ lib/turn.rb
@@ -0,0 +1,10 @@
+class Turn
+  attr_reader :player, :board, :acceptable_choices, :choice
+
+  def initialize (player, board)
+    @player = player
+    @board = board
+    @acceptable_choices = ["A", "B", "C", "D", "E", "F", "G"]
+    @choice = ""
+  end
+end
