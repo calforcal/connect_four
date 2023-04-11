76de99992d1a838957e24beb6b3d18b74be94d55
Michael Callahan <mcalla242@gmail.com>
2023-04-11 14:18:40 -0600

    Remove testing code, add in game class method calls

--- connect_four_runner.rb
+++ connect_four_runner.rb
@@ -3,20 +3,7 @@ require "./lib/player"
 require "./lib/board"
 require "./lib/node"
 require "./lib/turn"
+require "./lib/game"
 
-board = Board.new
-puts "What's your name?"
-name = gets.chomp
-player = Player.new(name)
-computer = Player.new("Computer", " O ")
-board.list_builder
-turn = Turn.new(player, board)
-cpu_turn = Turn.new(computer, board)
-puts "Welcome #{name}, let's play some Connect Four!"
-puts board.print_board
-while player.has_lost == false
-  turn.get_choice
-  turn.set_piece
-  cpu_turn.computer_choice
-  cpu_turn.set_piece
-end
+game = Game.new
+game.menu
