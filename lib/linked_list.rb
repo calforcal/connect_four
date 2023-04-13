1de8b7d168aedf116eb68c4e78226caf2c7510ec
Michael Callahan <mcalla242@gmail.com>
2023-04-13 09:44:53 -0600

    Remove unneeded methods

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -4,29 +4,6 @@ class Linked_List
     @head = head
   end
 
-  def count
-    count = 0
-    current_piece = @head
-    if current_piece == nil
-      @count += 0
-    elsif current_piece.node_below == nil
-      @count += 1
-    elsif
-      while current_piece.node_below != nil
-        current_piece = current_piece.node_below
-        @count += 1
-      end
-    end
-  end
-
-  def make_column
-    x = 0
-    while x < 6 do
-      add_blank
-      x += 1
-    end
-  end
-
   def append(piece)
     @head = Node.new(piece) if @head == nil
 
